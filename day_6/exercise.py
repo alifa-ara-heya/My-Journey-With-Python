93cdb9f48f9bdb849cd69459ccb1742eb52c6c19
Alifa Ara Heya <alifaaraheya@gmail.com>
2020-08-02 13:36:55 +0600

    ..

---
+++ day_6/exercise.py
@@ -0,0 +1,13 @@
+#Day_6: August/02/2020
+#In the name 0f Allah..
+#Me: Alifa Ara Heya
+
+radius = 30
+pi = 3.14
+area_of_a_circle = pi * radius**2
+circumference_of_a_circle = 2 * pi * radius
+
+print("Area of a circle: ", area_of_a_circle, "square meter.")
+print("Circumference of the circle is:", circumference_of_a_circle, "meter.")
+
+radius = input('Enter the radius: ')
---
+++ day_6/more_variables.py
@@ -0,0 +1,21 @@
+#Day_6: August/02/2020
+#In the name 0f Allah..
+#Me: Alifa Ara Heya
+
+skills= ['hTML', 'CSS', 'Python']
+person_info = {
+    'firstname' : 'Alifa',
+    'lastname' : 'Hiya',
+    'country' : 'Bangladesh',
+    'city' : 'Dhaka'
+}
+
+print(skills)
+print(person_info)
+
+#str to int:
+firstname = 'Alifa'
+print("First Name:", list(firstname))        #output: ['A', 'l', 'i', 'f', 'a']
+#or,
+firstname = list(firstname)
+print("First name in list:", firstname)
