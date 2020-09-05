8ff0b12a53daecd4c8ea490ed8a2e0d91532336d
Alifa Ara Heya <alifaaraheya@gmail.com>
2020-09-05 19:45:26 +0600

    ..

---
+++ day_30/n.py
@@ -0,0 +1,16 @@
+print(5//5)  # 1
+print(5//4)  # 1
+print(5//3)  # 1
+print(5//2)  # 2
+print(5//1)  # 5
+
+print(5%5)   # 0
+print(5%4)   # 1
+print(5%3)   # 2
+print(5%2)   # 1
+print(5%1)   # 0
+
+print(5//5)
+print((5/4) - (5//4)) # 0.25
+print((5/3) - (5//3))
+print((5/2) - (5//2))
